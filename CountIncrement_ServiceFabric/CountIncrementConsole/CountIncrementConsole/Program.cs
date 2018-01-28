cc3859c2af44be1c3922a679d4da22ca0e56eae6
FORDNA1\MSANGARA <madhufuture@gmail.com>
2018-01-28 09:33:14 -0500

    Storage data in both Blob and Azure Table Storage

---
+++ CountIncrement_ServiceFabric/CountIncrementConsole/CountIncrementConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CountIncrementConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+                if (i % 5 != 0) continue;
+                Console.WriteLine("Do you want to continue (y/n)");
+                var response = Console.ReadLine();
+                if (Convert.ToChar(response) == 'y')
+                {
+                    i = 0;
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
