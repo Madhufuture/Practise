146de29e2b1b7c30f9da823e54471b7ea422583c
madhufuture <madhufuture@gmail.com>
2018-05-15 22:49:28 -0400

    WinService using TopShelf and Hangfire

---
+++ WinServiceTopShelf/WinServiceTopShelf/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace WinServiceTopShelf
+{
+    public class Service
+    {
+        public void Start()
+        {
+            //TODO: Execute business logic from this method.
+            //CacheManagerBusiness.Execute();
+        }
+        public void Stop() { }
+    }
+}
