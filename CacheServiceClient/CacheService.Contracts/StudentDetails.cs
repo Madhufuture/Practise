129950b75c0ed79c6ee09bdf390eb8339606c242
madhufuture <madhufuture@gmail.com>
2019-04-04 09:45:13 -0400

    Azure Relay Service sample application

---
+++ CacheServiceClient/CacheService.Contracts/StudentDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheService.Contracts
+{
+    [Serializable]
+    [DataContract]
+    public class StudentDetails
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
---
+++ CacheServiceClient/CacheService.Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheService.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.AutoDetect;
+            var cacheService = new ServiceHost(typeof(CacheService));
+
+            cacheService.Open();
+            Console.WriteLine("Press ENTER to close");
+            Console.ReadLine();
+            cacheService.Close();
+        }
+    }
+}
